d643a11079770b07a0bd3ae413d068968b2766d5
ilknurakdal <ilknurakdal1@gmail.com>
2026-07-01 22:03:00 +0300

    Add password check function in script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function kontrol(){
+
+let sifre=document.getElementById("sifre").value;
+
+let sonuc=document.getElementById("sonuc");
+
+if(sifre=="1234567"){
+
+sonuc.style.color="lime";
+
+sonuc.innerHTML="🎉 Tebrikler! Sandık açıldı.";
+
+}
+
+else{
+
+sonuc.style.color="red";
+
+sonuc.innerHTML="❌ Şifre yanlış.";
+
+}
+
+}
